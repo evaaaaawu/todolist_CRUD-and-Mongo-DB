7c8c2a77665abb76a0663c84d162f68534bdd22e
evaaaaawu <eva19960223@gmail.com>
2022-12-16 17:21:41 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 // require packages used in the project
 const express = require('express')  //載入express
+const mongoose = require('mongoose')  //載入mongoose
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) //設定連線到mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 const port = 3000
 
 // routes setting
